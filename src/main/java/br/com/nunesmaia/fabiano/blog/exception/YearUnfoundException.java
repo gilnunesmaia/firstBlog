4459c93e5bb1a8aea7dad840edcaceb8d71adf55
Fabiano Nunesmaia <fanunesmaia@gmail.com>
2023-02-24 14:55:23 -0300

    Criação da exceção da regra de negócio.

---
+++ src/main/java/br/com/nunesmaia/fabiano/blog/exception/YearUnfoundException.java
@@ -0,0 +1,9 @@
+package br.com.nunesmaia.fabiano.blog.exception;
+
+
+public class YearUnfoundException extends Exception{
+
+    public YearUnfoundException() {
+        super("Year Unfounded");
+    }
+}
